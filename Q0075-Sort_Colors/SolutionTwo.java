903a002a61f4965b4497e5387e776f97721064d3
Zhe Lin <fslinzhe@hotmail.com>
2021-05-12 01:33:20 -0400

    added q75

--- Q0075-Sort_Colors/SolutionTwo.java
+++ Q0075-Sort_Colors/SolutionTwo.java
@@ -7,9 +7,9 @@ class SolutionTwo {
         {
             if(A[p] == 0)
             {
-                A[++k] = 2;    //move 2 backward
-                A[++j] = 1;    //move 1 backward
-                A[++i] = 0;    //move 0 backward
+                A[++k] = 2;    //move 2s backward
+                A[++j] = 1;    //move 1s backward
+                A[++i] = 0;    //move 0s backward
             }
             else if(A[p] == 1)
             {
