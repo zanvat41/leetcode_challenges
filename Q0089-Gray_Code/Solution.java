3fe17eff052ef8c0a3f434c19c590771131ca066
Zhe Lin <fslinzhe@hotmail.com>
2021-05-13 02:05:32 -0400

    added q89

---
+++ Q0089-Gray_Code/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> grayCode(int n) {
+        if(n==0){
+            List<Integer> result = new ArrayList<Integer>();
+            result.add(0);
+            return result;
+        }
+
+        List<Integer> result = grayCode(n-1);
+        int numToAdd = 1<<(n-1);
+
+        for(int i=result.size()-1; i>=0; i--){ //iterate from last to first
+            result.add(numToAdd+result.get(i));
+        }
+
+        return result;
+
+    }
+}
