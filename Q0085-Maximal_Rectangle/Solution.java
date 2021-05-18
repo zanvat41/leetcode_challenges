d59f9da9fd146140c4ad49baadd7d2b9c83b93de
Zhe Lin <fslinzhe@hotmail.com>
2021-05-17 22:57:05 -0400

    added q85

---
+++ Q0085-Maximal_Rectangle/Solution.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix.length == 0) return 0;
+        int maxarea = 0;
+        // dp[j] is depth(or reverse height) of the 1's
+        // at each row
+        int[] dp = new int[matrix[0].length];
+
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                // update the state of this row's histogram using the last row's histogram
+                // by keeping track of the number of consecutive ones
+
+                dp[j] = matrix[i][j] == '1' ? dp[j] + 1 : 0;
+            }
+            // update maxarea with the maximum area from this row's histogram
+            maxarea = Math.max(maxarea, largestRectangleArea(dp));
+        }
+        return maxarea;
+    }
+
+    public int largestRectangleArea(int[] heights) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(-1);
+        int length = heights.length;
+        int maxArea = 0;
+        for (int i = 0; i < length; i++) {
+            while ((stack.peek() != -1)
+                    && (heights[stack.peek()] >= heights[i])) {
+                int currentHeight = heights[stack.pop()];
+                int currentWidth = i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, currentHeight * currentWidth);
+            }
+            stack.push(i);
+        }
+        while (stack.peek() != -1) {
+            int currentHeight = heights[stack.pop()];
+            int currentWidth = length - stack.peek() - 1;
+            maxArea = Math.max(maxArea, currentHeight * currentWidth);
+        }
+        return maxArea;
+    }
+}
