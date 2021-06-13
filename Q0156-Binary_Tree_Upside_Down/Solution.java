2f4113946814e7feea0d47a1854677da0794c4e6
Zhe Lin <fslinzhe@hotmail.com>
2021-06-13 19:35:33 -0400

    added q156

---
+++ Q0156-Binary_Tree_Upside_Down/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        TreeNode node = root, parent = null, right = null;
+        while (node != null) {
+            TreeNode left = node.left;
+            node.left = right;
+            right = node.right;
+            node.right = parent;
+            parent = node;
+            node = left;
+        }
+        return parent;
+    }
+}
