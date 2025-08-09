7376725c489ce4850894ab2aecc894a45ea4a3fd
zanvat41 <fslinzhe@hotmail.com>
2025-08-09 09:24:18 +0800

    add q122

--- Q0101-0200/Q0122-Best_Time_to_Buy_and_Sell_Stock_II/Solution.java
+++ Q0101-0200/Q0122-Best_Time_to_Buy_and_Sell_Stock_II/Solution.java
@@ -1,11 +1,9 @@
-public class Solution {
-    public int maxProfit(int prices[]) {
-        int minprice = Integer.MAX_VALUE;
+class Solution {
+    public int maxProfit(int[] prices) {
         int maxprofit = 0;
-        for (int i = 0; i < prices.length; i++) {
-            if (prices[i] < minprice) minprice = prices[i];
-            else if (prices[i] - minprice > maxprofit) maxprofit = prices[i] -
-                    minprice;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
         }
         return maxprofit;
     }
