c5802b31c50b3d830fe6574d93dd92824945d346
zanvat41 <fslinzhe@hotmail.com>
2025-08-10 18:24:29 +0800

    add q392

---
+++ Q0392-Is_Subsequence/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length() > t.length()) return false;
+        int i = 0, j = 0;
+        while(i < s.length() && j < t.length()) {
+            if(s.charAt(i) == t.charAt(j)){
+                i++;
+            }
+            j++;
+        }
+        return i==s.length();
+    }
+}
