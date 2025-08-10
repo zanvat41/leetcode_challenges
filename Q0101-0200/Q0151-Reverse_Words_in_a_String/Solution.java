d15b3fd07913d4f985c19d32cc778ee366058131
zanvat41 <fslinzhe@hotmail.com>
2025-08-10 17:47:57 +0800

    update 151

--- Q0101-0200/Q0151-Reverse_Words_in_a_String/Solution.java
+++ Q0101-0200/Q0151-Reverse_Words_in_a_String/Solution.java
@@ -1,5 +1,18 @@
 class Solution {
     public String reverseWords(String s) {
+
+        String[] arr = s.split(" +");;
+        StringBuilder sb = new StringBuilder();
+        for(int i= arr.length-1;i>=0;i--){
+            sb.append(arr[i]);
+            sb.append(" ");
+        }
+        return sb.toString().trim();
+    }
+}
+/*
+class Solution {
+    public String reverseWords(String s) {
         StringBuilder res = new StringBuilder();
         for (int start = s.length() - 1; start >= 0; start--) {
             if (s.charAt(start) == ' ') continue;
@@ -10,3 +23,4 @@ class Solution {
         return res.toString().trim();
     }
 }
+/*
