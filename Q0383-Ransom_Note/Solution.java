1fd23df1eacbfcde0ef45e51d8996f7e45a940c2
zanvat41 <fslinzhe@hotmail.com>
2025-08-11 17:26:43 +0800

    add q383

---
+++ Q0383-Ransom_Note/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] map = new int[26];
+        for(char c : magazine.toCharArray()) {
+            map[c-'a'] ++;
+        }
+        for(char c : ransomNote.toCharArray()) {
+            map[c - 'a']--;
+        }
+
+        for(int i = 0; i < 26; i++) {
+            if(map[i] < 0) return false;
+        }
+        return true;
+    }
+}
