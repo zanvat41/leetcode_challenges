8b123cba5bb71b6831fad49df42bea596678831d
zanvat41 <fslinzhe@hotmail.com>
2025-08-14 02:55:41 +0800

    add q637

--- Q0637-Average_of_Levels_in_Binary_Tree/Solution.java
+++ Q0637-Average_of_Levels_in_Binary_Tree/Solution.java
@@ -36,3 +36,29 @@ class Solution {
         average(t.right, i + 1, sum, count);
     }
 }
+
+/*
+public class Solution {
+    public List < Double > averageOfLevels(TreeNode root) {
+        List < Double > res = new ArrayList < > ();
+        Queue < TreeNode > queue = new LinkedList < > ();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            long sum = 0, count = 0;
+            Queue < TreeNode > temp = new LinkedList < > ();
+            while (!queue.isEmpty()) {
+                TreeNode n = queue.remove();
+                sum += n.val;
+                count++;
+                if (n.left != null)
+                    temp.add(n.left);
+                if (n.right != null)
+                    temp.add(n.right);
+            }
+            queue = temp;
+            res.add(sum * 1.0 / count);
+        }
+        return res;
+    }
+}
+ */
