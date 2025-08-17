b5fdeeb4ad0fdf93b4d7f01727806ff974a59cc0
zanvat41 <fslinzhe@hotmail.com>
2025-08-18 02:16:24 +0800

    add q708

---
+++ Q0301-2000/Q0708-Insert_into_a_Sorted_Circular_Linked_List/Solution.java
@@ -0,0 +1,55 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _next) {
+        val = _val;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node insert(Node head, int insertVal) {
+        if (head == null) {
+            Node newNode = new Node(insertVal, null);
+            newNode.next = newNode;
+            return newNode;
+        }
+
+        Node prev = head;
+        Node curr = head.next;
+        boolean toInsert = false;
+
+        do {
+            if (prev.val <= insertVal && insertVal <= curr.val) {
+                // Case 1). min <= new node value <= max
+                toInsert = true;
+            } else if (prev.val > curr.val) {
+                // Case 2). new node value <= min or new node value >= max
+                if (insertVal >= prev.val || insertVal <= curr.val)
+                    toInsert = true;
+            }
+
+            if (toInsert) {
+                prev.next = new Node(insertVal, curr);
+                return head;
+            }
+
+            prev = curr;
+            curr = curr.next;
+        } while (prev != head);
+
+        // Case 3). All nodes in the original list have same value
+        prev.next = new Node(insertVal, curr);
+        return head;
+    }
+}
