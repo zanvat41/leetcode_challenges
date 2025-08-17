ddc538dd72ad01c60899f1f86407a6f8078c92b8
zanvat41 <fslinzhe@hotmail.com>
2025-08-17 19:03:03 +0800

    add q772

---
+++ Q0301-2000/Q0772-Basic_Calculator_III/Solution.java
@@ -0,0 +1,59 @@
+class Solution {
+    private int evaluate(char operator, int x, int y) {
+        if (operator == '+') {
+            return x;
+        } else if (operator == '-') {
+            return -x;
+        } else if (operator == '*') {
+            return x * y;
+        }
+
+        return x / y;
+    }
+
+    private int solve(String s, int[] i) {
+        Stack<Integer> stack = new Stack<>();
+        int curr = 0;
+        char previousOperator = '+';
+
+        while (i[0] < s.length()) {
+            char c = s.charAt(i[0]);
+            if (c == '(') {
+                i[0]++;
+                curr = solve(s, i);
+            } else if (Character.isDigit(c)) {
+                curr = curr * 10 + Character.getNumericValue(c);
+            } else {
+                if (previousOperator == '*' || previousOperator == '/') {
+                    stack.push(evaluate(previousOperator, stack.pop(), curr));
+                } else {
+                    stack.push(evaluate(previousOperator, curr, 0));
+                }
+
+                if (c == ')') {
+                    break;
+                }
+
+                curr = 0;
+                previousOperator = c;
+            }
+
+            i[0]++;
+        }
+
+        int ans = 0;
+        for (int num: stack) {
+            ans += num;
+        }
+
+        return ans;
+    }
+
+    public int calculate(String s) {
+        s += "@";
+        // use int array of size 1 instead of int
+        // because we need to keep track of the index after recursions
+        int[] i = new int[1];
+        return solve(s, i);
+    }
+}
