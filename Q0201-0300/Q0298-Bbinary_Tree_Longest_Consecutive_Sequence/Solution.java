f7ace8c29a5f825d45e5c264515651501b98c9c5
zanvat41 <fslinzhe@hotmail.com>
2025-08-18 17:34:16 +0800

    add q298

---
+++ Q0201-0300/Q0298-Bbinary_Tree_Longest_Consecutive_Sequence/Solution.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int longestConsecutive(TreeNode root) {
+        return dfs(root, null, 0);
+    }
+
+    private int dfs(TreeNode p, TreeNode parent, int length) {
+        if (p == null) return length;
+        length = (parent != null && p.val == parent.val + 1) ? length + 1 : 1;
+        return Math.max(length, Math.max(dfs(p.left, p, length),
+                dfs(p.right, p, length)));
+    }
+}
