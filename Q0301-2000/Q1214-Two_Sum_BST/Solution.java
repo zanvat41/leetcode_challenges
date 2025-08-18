8c611960ec51ce01f1c2ddb2bafeac1414eb190a
zanvat41 <fslinzhe@hotmail.com>
2025-08-19 01:40:23 +0800

    add q1214

---
+++ Q0301-2000/Q1214-Two_Sum_BST/Solution.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+
+    public boolean binarySearch(TreeNode root, int target) {
+        if (root == null) {
+            return false;
+        }
+        if (root.val == target) {
+            return true;
+        }
+        return root.val < target ? binarySearch(root.right, target) : binarySearch(root.left, target);
+    }
+
+    public boolean twoSumBSTs(TreeNode root1, TreeNode root2, int target) {
+        return dfs(root1, root2, target);
+    }
+
+    public boolean dfs(TreeNode root1, TreeNode root2, int target) {
+        if (root1 == null) {
+            return false;
+        }
+        if (binarySearch(root2, target - root1.val)) {
+            return true;
+        }
+
+        return dfs(root1.left, root2, target) || dfs(root1.right, root2, target);
+    }
+}
