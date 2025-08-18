acb8cefab2dd2600328ea10f9dac7dd755eab0cd
zanvat41 <fslinzhe@hotmail.com>
2025-08-19 02:26:25 +0800

    add q277

---
+++ Q0201-0300/Q0277-Find_the_Celebrity/Solution.java
@@ -0,0 +1,40 @@
+/* The knows API is defined in the parent class Relation.
+      boolean knows(int a, int b); */
+
+public class Solution extends Relation {
+
+    private int numberOfPeople;
+    private Map<Pair<Integer, Integer>, Boolean> cache = new HashMap<>();
+
+    @Override
+    public boolean knows(int a, int b) {
+        if (!cache.containsKey(new Pair(a, b))) {
+            cache.put(new Pair(a, b), super.knows(a, b));
+        }
+        return cache.get(new Pair(a, b));
+    }
+
+    public int findCelebrity(int n) {
+        numberOfPeople = n;
+        int celebrityCandidate = 0;
+        for (int i = 1; i < n; i++) {
+            if (knows(celebrityCandidate, i)) {
+                celebrityCandidate = i;
+            }
+        }
+        if (isCelebrity(celebrityCandidate)) {
+            return celebrityCandidate;
+        }
+        return -1;
+    }
+
+    private boolean isCelebrity(int i) {
+        for (int j = 0; j < numberOfPeople; j++) {
+            if (i == j) continue; // Don't ask if they know themselves.
+            if (knows(i, j) || !knows(j, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
