ce9516f952c5f91effde94b1ab6ae25d5e7f5e73
zanvat41 <fslinzhe@hotmail.com>
2025-08-19 18:38:30 +0800

    add q1136

---
+++ Q0301-2000/Q1136-Parallel_Courses/Solution.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int minimumSemesters(int N, int[][] relations) {
+        int[] inCount = new int[N + 1]; // or indegree
+        List<List<Integer>> graph = new ArrayList<>(N + 1);
+        for (int i = 0; i < N + 1; ++i) {
+            graph.add(new ArrayList<Integer>());
+        }
+        for (int[] relation : relations) {
+            graph.get(relation[0]).add(relation[1]);
+            inCount[relation[1]]++;
+        }
+        int step = 0;
+        int studiedCount = 0;
+        List<Integer> bfsQueue = new ArrayList<>();
+        for (int node = 1; node < N + 1; node++) {
+            if (inCount[node] == 0) {
+                bfsQueue.add(node);
+            }
+        }
+        // start learning with BFS
+        while (!bfsQueue.isEmpty()) {
+            // start new semester
+            step++;
+            List<Integer> nextQueue = new ArrayList<>();
+            for (int node : bfsQueue) {
+                studiedCount++;
+                for (int endNode : graph.get(node)) {
+                    inCount[endNode]--;
+                    // if all prerequisite courses learned
+                    if (inCount[endNode] == 0) {
+                        nextQueue.add(endNode);
+                    }
+                }
+            }
+            bfsQueue = nextQueue;
+        }
+
+        // check if learn all courses
+        return studiedCount == N ? step : -1;
+    }
+}
