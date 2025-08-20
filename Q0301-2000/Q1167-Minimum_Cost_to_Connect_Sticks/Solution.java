6b762bfb9d0e888bf28b0f7d06630575b5de9cef
zanvat41 <fslinzhe@hotmail.com>
2025-08-20 18:17:04 +0800

    add q1167

---
+++ Q0301-2000/Q1167-Minimum_Cost_to_Connect_Sticks/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int connectSticks(int[] sticks) {
+        if(sticks.length < 2) return 0;
+
+        int totalCost = 0;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        // add all sticks to the min heap.
+        for (int stick : sticks) {
+            pq.add(stick);
+        }
+
+        // combine two of the smallest sticks until we are left with just one.
+        while (pq.size() > 1) {
+            int stick1 = pq.remove();
+            int stick2 = pq.remove();
+
+            int cost = stick1 + stick2;
+            totalCost += cost;
+
+            pq.add(cost);
+        }
+
+        return totalCost;
+    }
+}
