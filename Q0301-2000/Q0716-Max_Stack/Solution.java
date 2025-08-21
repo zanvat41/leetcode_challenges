24a03eb0637c6ec6c46da8b4239808ef4b3db317
zanvat41 <fslinzhe@hotmail.com>
2025-08-22 02:59:24 +0800

    add q716

---
+++ Q0301-2000/Q0716-Max_Stack/Solution.java
@@ -0,0 +1,61 @@
+class MaxStack {
+    private Stack<int[]> stack;
+    private Queue<int[]> heap;
+    private Set<Integer> removed;
+    private int id;
+
+    public MaxStack() {
+        stack = new Stack<>();
+        heap = new PriorityQueue<>((a, b) -> b[0] - a[0] == 0 ? b[1] - a[1] : b[0] - a[0]);
+        removed = new HashSet<>();
+    }
+
+    public void push(int x) {
+        stack.add(new int[] { x, id });
+        heap.add(new int[] { x, id });
+        id++;
+    }
+
+    public int pop() {
+        while (removed.contains(stack.peek()[1])) {
+            stack.pop();
+        }
+        int[] top = stack.pop();
+        removed.add(top[1]);
+        return top[0];
+    }
+
+    public int top() {
+        while (removed.contains(stack.peek()[1])) {
+            stack.pop();
+        }
+        return stack.peek()[0];
+    }
+
+    public int peekMax() {
+        while (removed.contains(heap.peek()[1])) {
+            heap.poll();
+        }
+        return heap.peek()[0];
+
+    }
+
+    public int popMax() {
+        while (removed.contains(heap.peek()[1])) {
+            heap.poll();
+        }
+        int[] top = heap.poll();
+        removed.add(top[1]);
+        return top[0];
+    }
+}
+
+/**
+ * Your MaxStack object will be instantiated and called as such:
+ * MaxStack obj = new MaxStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.peekMax();
+ * int param_5 = obj.popMax();
+ */
