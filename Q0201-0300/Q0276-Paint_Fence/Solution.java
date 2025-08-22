751d3ee03cbf2717ac507d78c6cc07b5a04e921a
zanvat41 <fslinzhe@hotmail.com>
2025-08-22 18:19:23 +0800

    add q276

---
+++ Q0201-0300/Q0276-Paint_Fence/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numWays(int n, int k) {
+        if (n == 1) return k;
+
+        int twoPostsBack = k;
+        int onePostBack = k * k;
+
+        for (int i = 3; i <= n; i++) {
+            int curr = (k - 1) * (onePostBack + twoPostsBack);
+            twoPostsBack = onePostBack;
+            onePostBack = curr;
+        }
+
+        return onePostBack;
+    }
+}
