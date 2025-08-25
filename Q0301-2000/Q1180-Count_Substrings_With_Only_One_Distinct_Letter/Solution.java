4a3e638905d354b6d53674d0bcb1f7fe7ce07779
zanvat41 <fslinzhe@hotmail.com>
2025-08-25 19:11:41 +0800

    add q1538

--- Q0301-2000/Q1180-Count_Substrings_With_Only_One_Distinct_Letter/Solution.java
+++ Q0301-2000/Q1180-Count_Substrings_With_Only_One_Distinct_Letter/Solution.java
@@ -1,1 +1,13 @@
-count-substrings-with-only-one-distinct-lette
+class Solution {
+    public int countLetters(String s) {
+        int total = 0;
+        for(int left = 0, right = 0; right <= s.length(); right++) {
+            if(right == s.length() || s.charAt(left) != s.charAt(right)) {
+                int lenSubstring = right - left;
+                total += (1 + lenSubstring) * lenSubstring / 2;
+                left = right;
+            }
+        }
+        return total;
+    }
+}
